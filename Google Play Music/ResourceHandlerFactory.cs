184df4a7348d378ee915bbfa72cec0686cc9ee36
Samuel Attard <samuel.r.attard@gmail.com>
2015-10-27 14:06:18 +1100

    Inject JS on the first JS file, not a specific one

--- Google Play Music/ResourceHandlerFactory.cs
+++ Google Play Music/ResourceHandlerFactory.cs
@@ -8,18 +8,19 @@ namespace Google_Play_Music
 {
     class GPMResouceHandlerFactory : IResourceHandlerFactory
     {
-
+        private bool firstJSOnly = true;
         public IResourceHandler GetResourceHandler(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request)
         {
-            if (Regex.Match(request.Url, @"polymer_srcs.js", RegexOptions.IgnoreCase).Success)
+            if (Regex.Match(request.Url, @".js", RegexOptions.IgnoreCase).Success && Regex.Match(request.Url, @"http", RegexOptions.IgnoreCase).Success && firstJSOnly)
             {
+                firstJSOnly = false;
                 using (WebClient webClient = new WebClient())
                 {
                     // These are the JS files to inject into GPM
                     string dark_theme = Google_Play_Music.Properties.Resources.dark_theme;
                     string custom_interface = Google_Play_Music.Properties.Resources.custom_interface;
                     string mini_player = Google_Play_Music.Properties.Resources.mini_player;
-                    return ResourceHandler.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(webClient.DownloadString(request.Url) + dark_theme + custom_interface + mini_player)), webClient.ResponseHeaders["Content-Type"]);
+                    return ResourceHandler.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(webClient.DownloadString(request.Url) + ";" + dark_theme + custom_interface + mini_player)), webClient.ResponseHeaders["Content-Type"]);
                 }
             }
             return null;
