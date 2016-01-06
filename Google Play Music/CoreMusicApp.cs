ae03fe3368005e3b6645226028d84914e02ad6c0
Samuel Attard <samuel.r.attard@gmail.com>
2016-01-06 23:14:10 +1300

    Version bump

--- Google Play Music/CoreMusicApp.cs
+++ Google Play Music/CoreMusicApp.cs
@@ -15,7 +15,7 @@ namespace Google_Play_Music
     public partial class CoreMusicApp : MaterialForm
     {
 
-        private const string CURRENT_VERSION = "2.0.1";
+        private const string CURRENT_VERSION = "2.0.2";
         private MaterialSkinManager skin;
         private Size rolling_size;
         private Size last_size;
