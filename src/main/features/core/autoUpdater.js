0efe4c68f239cbd86ab208d95df1604733053e57
Samuel Attard <samuel.r.attard@gmail.com>
2016-02-14 16:07:35 +1300

    Added a sanity check to ensure the update server is up and running

--- src/main/features/core/autoUpdater.js
+++ src/main/features/core/autoUpdater.js
@@ -1,47 +1,61 @@
 import { app, autoUpdater } from 'electron';
+import http from 'http';
 
 let platform = 'win';
 if (process.platform === 'darwin') {
   platform = 'osx';
 }
 
-try {
-  autoUpdater.setFeedURL(`http://ut.samuel.ninja:6069/update/${platform}/${app.getVersion()}`);
+const setUpAutoUpdate = () => {
+  try {
+    autoUpdater.setFeedURL(`http://update.googleplaymusicdesktopplayer.com/update/${platform}/${app.getVersion()}`);
 
-  autoUpdater.on('checking-for-update', () => {
-    // Do something
-  });
+    autoUpdater.on('checking-for-update', () => {
+      // Do something
+    });
 
-  autoUpdater.on('update-available', () => {
-    // Do something
-  });
+    autoUpdater.on('update-available', () => {
+      // Do something
+    });
 
-  autoUpdater.on('update-not-available', () => {
-    Emitter.sendToAll('update:available');
-    setTimeout(() => {
-      autoUpdater.checkForUpdates();
-    }, 300000);
-  });
-
-  let update = false;
-  autoUpdater.on('update-downloaded', () => {
-    Emitter.sendToAll('update:available');
-    update = true;
-  });
-
-  Emitter.on('update:trigger', () => {
-    if (update) {
-      autoUpdater.quitAndInstall();
-    }
-  });
-
-  Emitter.on('update:wait', () => {
-    setTimeout(() => {
+    autoUpdater.on('update-not-available', () => {
       Emitter.sendToAll('update:available');
-    }, 12000);
-  });
+      setTimeout(() => {
+        autoUpdater.checkForUpdates();
+      }, 300000);
+    });
 
-  autoUpdater.checkForUpdates();
-} catch (e) {
-  Settings.set('woahError', e);
-}
+    let update = false;
+    autoUpdater.on('update-downloaded', () => {
+      Emitter.sendToAll('update:available');
+      update = true;
+    });
+
+    Emitter.on('update:trigger', () => {
+      if (update) {
+        autoUpdater.quitAndInstall();
+      }
+    });
+
+    Emitter.on('update:wait', () => {
+      setTimeout(() => {
+        Emitter.sendToAll('update:available');
+      }, 12000);
+    });
+
+    autoUpdater.checkForUpdates();
+  } catch (e) {
+    Settings.set('woahError', e);
+  }
+};
+
+const checkUpdateServer = () => {
+  http.get('http://update.googleplaymusicdesktopplayer.com', () => {
+    setUpAutoUpdate();
+  }).on('error', () => {
+    console.log('################### !! Update server down !! ##################'); // eslint-disable-line
+    setTimeout(checkUpdateServer, 120000);
+  });
+};
+
+checkUpdateServer();
