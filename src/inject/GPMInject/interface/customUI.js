a189959a5866b8836a8e30226c5a9fe1f8eeae72
Ondřej Hruška <ondra@ondrovo.com>
2016-03-07 22:06:14 +0100

    Fixed problem with search clear button

--- src/inject/GPMInject/interface/customUI.js
+++ src/inject/GPMInject/interface/customUI.js
@@ -21,6 +21,13 @@ const style = (elementSelector, styleObject) => {
   });
 };
 
+const cssRule = (styles) => {
+  const tag = document.createElement('style');
+  tag.type = 'text/css';
+  tag.appendChild(document.createTextNode(styles));
+  document.head.appendChild(tag);
+};
+
 window.wait(() => {
   // Top left account control buttons
   hide('#material-one-right #gb > div > div > div:not(:last-child)');
@@ -58,24 +65,32 @@ window.wait(() => {
   const back = document.createElement('paper-icon-button');
   back.setAttribute('onclick', 'history.back()');
   back.setAttribute('icon', 'arrow-back');
-  back.setAttribute('id', 'back-button');
+  back.setAttribute('id', 'backButton');
   back.setAttribute('class', 'x-scope paper-icon-button-0');
   document.querySelector('#material-one-middle > sj-search-box').insertBefore(back, null);
 
-  style('#back-button', {
-      'position': 'absolute',
-      'right': '3px',
-      'top': '1px',
-      'width': '46px',
-      'height': '46px',
-      'opacity': '0',
-      'transition': 'opacity 0.2s ease-in-out'
+  style('#backButton', {
+    position: 'absolute',
+    right: '3px',
+    top: '1px',
+    width: '46px',
+    height: '46px',
+    opacity: '0',
+    transition: 'opacity 0.2s ease-in-out',
   });
+
+  // Make sure the clearSearch button is above the arrow
+  style('sj-search-box #clearButton', { 'z-index': 10 });
+
+  // Hide icon if search box has query
+  cssRule('sj-search-box[has-query] #backButton {opacity: 0 !important}');
 
   // Ideally we should listen for the URL change
   // 'hashchange' does not seem to work :(
   setInterval(() => {
-    if (location.href.indexOf('https://play.google.com/music/listen#/now') === 0) {
+    const homePage = (location.href.indexOf('https://play.google.com/music/listen#/now') === 0);
+    const searching = (document.querySelector('sj-search-box input').value !== '');
+    if (homePage || searching) {
       back.style.opacity = 0;
     } else {
       back.style.opacity = 1;
