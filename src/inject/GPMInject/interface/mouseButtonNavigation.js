987cfdf4323b4550d0ee31f71941475cbb8b59ba
Samuel Attard <samuel.r.attard@gmail.com>
2016-03-11 20:09:37 +1100

    Fixes #470, goddam garbage collection

--- src/inject/GPMInject/interface/mouseButtonNavigation.js
+++ src/inject/GPMInject/interface/mouseButtonNavigation.js
@@ -1,12 +1,11 @@
 import { remote } from 'electron';
 
 const window = remote.getCurrentWindow();
-const webContents = remote.getCurrentWebContents();
 
 window.on('app-command', (e, cmd) => {
   if (cmd === 'browser-backward') {
-    webContents.goBack();
+    remote.getCurrentWebContents().goBack();
   } else if (cmd === 'browser-forward') {
-    webContents.goForward();
+    remote.getCurrentWebContents().goForward();
   }
 });
