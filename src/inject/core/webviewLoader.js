23dbf721e83c24b4471478262bcabaf5c2af2f50
Samuel Attard <samuel.r.attard@gmail.com>
2016-03-13 01:10:24 +1100

    Save and restore the page you are on when quitting / restoring the player

--- src/inject/core/webviewLoader.js
+++ src/inject/core/webviewLoader.js
@@ -7,14 +7,25 @@ if (webview) {
   webview.addEventListener('did-start-loading', () => {
     if (once) {
       once = false;
+      webview.src = Settings.get('lastPage', 'https://play.google.com/music/listen');
       document.body.setAttribute('loading', 'loading');
     }
   });
+
+  const savePage = (url) => {
+    Emitter.fire('settings:set', {
+      key: 'lastPage',
+      value: url.url,
+    });
+  };
 
   webview.addEventListener('dom-ready', () => {
     setTimeout(() => {
       document.body.removeAttribute('loading');
       webview.focus();
+      webview.addEventListener('did-navigate', savePage);
+      webview.addEventListener('did-navigate-in-page', savePage);
+
       window.addEventListener('beforeunload', () => {
         remote.getCurrentWindow().removeAllListeners();
       });
