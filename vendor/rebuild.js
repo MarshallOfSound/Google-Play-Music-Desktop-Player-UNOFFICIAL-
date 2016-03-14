9220c263c29c3aa69ad0520868458b65629db4c2
Samuel Attard <samuel.r.attard@gmail.com>
2016-03-15 01:31:28 +1300

    Fix mac usage of rebuild

--- vendor/rebuild.js
+++ vendor/rebuild.js
@@ -2,19 +2,23 @@ const spawn = require('child_process').spawn;
 
 console.log('Rebuilding native modules for electron'); // eslint-disable-line
 
-const build = spawn((process.platform === 'win32' ? 'rebuild.bat' : 'rebuild.sh'), {
+const build = spawn((process.platform === 'win32' ? 'rebuild.bat' : './rebuild.sh'), {
   cwd: __dirname,
 });
 
 build.stdout.on('data', (data) => {
-  console.log(data); // eslint-disable-line
+  console.log(data.toString()); // eslint-disable-line
 });
 
 build.stderr.on('data', (data) => {
-  console.error(data); // eslint-disable-line
+  console.error(data.toString()); // eslint-disable-line
 });
 
 build.on('exit', () => {
   console.log('Rebuild complete');  // eslint-disable-line
   // Who cares
+});
+
+build.on('error', (err) => {
+  console.error(err.toString()); // eslint-disable-line
 });
