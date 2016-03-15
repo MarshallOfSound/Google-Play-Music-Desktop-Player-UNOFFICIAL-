e6c17857ee1ac4de84343077f451d208b67a4e57
Samuel Attard <samuel.r.attard@gmail.com>
2016-03-15 19:29:40 +1100

    Only check whether to uninstall once
    Fixes #598

--- src/old_win32.js
+++ src/old_win32.js
@@ -2,8 +2,9 @@
 // But it needs to be here to handle the upgrade process from the old GPMDP
 import { exec } from 'child_process';
 
-if (process.platform === 'win32') {
+if (process.platform === 'win32' && !Settings.get('checkedForOldVersion', false)) {
   exec('wmic product where Vendor="MarshallOfSound" get PackageCache /format:value', (err, out) => {
+    Settings.set('checkedForOldVersion', true);
     const MSIPath = out.replace(/(?:\r\n|\r|\n)/g, '').replace('PackageCache=', '');
     if (MSIPath.split('.msi').length > 1) {
       Emitter.once('uninstall:confirm', () => {
