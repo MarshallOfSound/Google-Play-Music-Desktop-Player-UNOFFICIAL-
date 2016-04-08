209166f5e5672dc2d1c345e3126115c80f46472f
Matt Fox <mcfox1321@gmail.com>
2016-04-07 18:40:35 -0700

    Factoring and cleanup of 8022935af5df569dea84bd050430ba7a94e340fb

--- src/main/features/linux/mediaKeysDBus.js
+++ src/main/features/linux/mediaKeysDBus.js
@@ -1,39 +1,28 @@
 import DBus from 'dbus';
 
+function registerBindings(desktopEnv, session){
+  session.getInterface('org.'+desktopEnv+'.SettingsDaemon', '/org/'+desktopEnv+'/SettingsDaemon/MediaKeys','org.'+desktopEnv+'.SettingsDaemon.MediaKeys', (err, iface) => {
+    if (!err) {
+      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
+        switch (keyName) {
+          case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
+          case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
+          case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
+          case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
+          default: return;
+        }
+      });
+      iface.GrabMediaPlayerKeys(0, 'org.'+desktopEnv+'.SettingsDaemon.MediaKeys'); // eslint-disable-line
+    }
+  });
+}
+
 try {
   const dbus = new DBus();
   const session = dbus.getBus('session');
-
-  session.getInterface('org.gnome.SettingsDaemon', '/org/gnome/SettingsDaemon/MediaKeys',
-  'org.gnome.SettingsDaemon.MediaKeys', (err, iface) => {
-    if (!err) {
-      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
-        switch (keyName) {
-          case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
-          case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
-          case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
-          case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
-          default: return;
-        }
-      });
-      iface.GrabMediaPlayerKeys(0, 'org.gnome.SettingsDaemon.MediaKeys'); // eslint-disable-line
-    }
-  });
-  session.getInterface('org.mate.SettingsDaemon', '/org/mate/SettingsDaemon/MediaKeys',
-  'org.mate.SettingsDaemon.MediaKeys', (err, iface) => {
-    if (!err) {
-      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
-        switch (keyName) {
-          case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
-          case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
-          case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
-          case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
-          default: return;
-        }
-      });
-      iface.GrabMediaPlayerKeys(0, 'org.mate.SettingsDaemon.MediaKeys'); // eslint-disable-line
-    }
-  });
+  
+  registerBindings('gnome', session);
+  registerBindings('mate', session);
 } catch (e) {
   // do nothing.
 }
