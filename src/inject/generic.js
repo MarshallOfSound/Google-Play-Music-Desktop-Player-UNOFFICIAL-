5c24c63d78761dde80c69de7e4bb5f500b4ae496
Samuel Attard <samuel.r.attard@gmail.com>
2016-04-19 00:22:11 +1000

    Wait for DOMContentLoaded instead of the body element

--- src/inject/generic.js
+++ src/inject/generic.js
@@ -7,30 +7,27 @@ Settings.uncouple();
 
 require(`./${process.platform}`);
 
-const waitForBody = setInterval(() => {
-  if (document.body) {
-    clearInterval(waitForBody);
-    require('./windowThemeHandler');
-    setTimeout(() => require('electron').remote.getCurrentWindow().show(), 100);
+document.addEventListener('DOMContentLoaded', () => {
+  require('./windowThemeHandler');
+  setTimeout(() => require('electron').remote.getCurrentWindow().show(), 100);
 
-    const nativeFrameAtLaunch = Settings.get('nativeFrame');
+  const nativeFrameAtLaunch = Settings.get('nativeFrame');
 
-    document.body.classList.toggle('native-frame', nativeFrameAtLaunch);
+  document.body.classList.toggle('native-frame', nativeFrameAtLaunch);
 
-    Emitter.on('window:fullscreen', (event, state) => {
-      if (nativeFrameAtLaunch) return;
-      if (state.state) document.body.classList.add('native-frame');
-      if (!state.state) document.body.classList.remove('native-frame');
-    });
+  Emitter.on('window:fullscreen', (event, state) => {
+    if (nativeFrameAtLaunch) return;
+    if (state.state) document.body.classList.add('native-frame');
+    if (!state.state) document.body.classList.remove('native-frame');
+  });
 
-    document.addEventListener('dragover', (event) => {
-      event.preventDefault();
-      return false;
-    }, false);
+  document.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    return false;
+  }, false);
 
-    document.addEventListener('drop', (event) => {
-      event.preventDefault();
-      return false;
-    }, false);
-  }
-}, 10);
+  document.addEventListener('drop', (event) => {
+    event.preventDefault();
+    return false;
+  }, false);
+});
