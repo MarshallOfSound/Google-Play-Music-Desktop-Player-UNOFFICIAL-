462c73882484108d19eeb74da4f94fe7567e2b59
Jesse Ostrander <jesse@davisonsoftware.io>
2016-05-29 19:09:58 -0400

    50% of the fix for scroll volume in mini-player

--- src/inject/GPMWebView/interface/mini.js
+++ src/inject/GPMWebView/interface/mini.js
@@ -10,6 +10,9 @@ window.wait(() => {
   if (Settings.get('miniAlwaysShowSongInfo', false)) {
     document.body.setAttribute('controls', 'controls');
   }
+  if (Settings.get('miniUseScrollVolume', false)) {
+    window.GPM.mini.setScrollVolume(true);
+  }
 
   window.GPM.mini.on('enable', () => {
     Emitter.fire('mini', { state: true });
@@ -68,4 +71,8 @@ Emitter.on('miniAlwaysOnTop', (event, state) => {
   if (mini) {
     remote.getCurrentWindow().setAlwaysOnTop(state.state);
   }
+});
+Emitter.on('miniUseScrollVolume', (event, state) => {
+  console.log(state.state); // eslint-disable-line
+  window.GPM.mini.setScrollVolume(state.state);
 });
