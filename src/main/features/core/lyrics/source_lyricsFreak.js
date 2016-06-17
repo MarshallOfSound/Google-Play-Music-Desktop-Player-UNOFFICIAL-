b3ba3f0ebc02b17e985b9a67e14664c47a87610f
Samuel Attard <samuel.r.attard@gmail.com>
2016-06-18 00:11:13 +1000

    Catch errors when searching in lyricsFreak

--- src/main/features/core/lyrics/source_lyricsFreak.js
+++ src/main/features/core/lyrics/source_lyricsFreak.js
@@ -5,29 +5,33 @@ const fetchLyricsPage = (title, artist) =>
   new Promise((resolve, reject) => {
     const search = `http://www.lyricsfreak.com/search.php?a=search&type=song&q=${title.replace(/ /g, '+')}`;
     request(search, (err, resp) => {
-      if (err) return reject(err);
-      const HTML = JSON.stringify(resp.body);
-      let lyricsTable = HTML.split('tbody')[1];
-      lyricsTable = lyricsTable.replace(/\\r/g, '');
-      const trackRegEx = /<tr>[\s\S]+?><a[\s\S]+?&middot;[&nbsp;]+(.+?)<[\s\S]+?<a href=\\"(.+?)\\"[\s\S]+?>(.+?) lyrics<\/a>[\s\S]+?<\/tr>/g;
-      let match = trackRegEx.exec(lyricsTable);
-      const tracks = [];
-      while (match !== null) {
-        const track = {
-          artist: match[1].trim(),
-          url: `http://www.lyricsfreak.com${match[2].trim()}`,
-          track: match[3].trim(),
-        };
-        tracks.push(track);
-        match = trackRegEx.exec(lyricsTable);
-      }
-      for (let i = 0; i < tracks.length; i++) {
-        const track = tracks[i];
-        if (track.artist.toLowerCase() === artist.toLowerCase()) {
-          return resolve(track.url);
+      try {
+        if (err) return reject(err);
+        const HTML = JSON.stringify(resp.body);
+        let lyricsTable = HTML.split('tbody')[1];
+        lyricsTable = lyricsTable.replace(/\\r/g, '');
+        const trackRegEx = /<tr>[\s\S]+?><a[\s\S]+?&middot;[&nbsp;]+(.+?)<[\s\S]+?<a href=\\"(.+?)\\"[\s\S]+?>(.+?) lyrics<\/a>[\s\S]+?<\/tr>/g;
+        let match = trackRegEx.exec(lyricsTable);
+        const tracks = [];
+        while (match !== null) {
+          const track = {
+            artist: match[1].trim(),
+            url: `http://www.lyricsfreak.com${match[2].trim()}`,
+            track: match[3].trim(),
+          };
+          tracks.push(track);
+          match = trackRegEx.exec(lyricsTable);
         }
+        for (let i = 0; i < tracks.length; i++) {
+          const track = tracks[i];
+          if (track.artist.toLowerCase() === artist.toLowerCase()) {
+            return resolve(track.url);
+          }
+        }
+      } catch (e) {
+        // Who cares
       }
-      reject('Could not find track');
+      return reject('Could not find track');
     });
   });
 
