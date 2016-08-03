a06ccfbe25c78050394d2eb1a5a0148796253a31
Samuel Attard <samuel.r.attard@gmail.com>
2016-08-03 18:48:28 +1000

    Show the song name when paused with a 'paused' modifier.  Fixes #1364

--- src/main/features/core/windowTitleInfo.js
+++ src/main/features/core/windowTitleInfo.js
@@ -5,11 +5,20 @@ PlaybackAPI.on('change:song', (songInfo) => {
   global.appIcon.setToolTip(newString);
   WindowManager.get(global.mainWindowID).setTitle(newString);
 });
-PlaybackAPI.on('change:state', (state) => {
-  const songInfo = PlaybackAPI.currentSong();
-  const newString = (state)
-  ? `${(songInfo.title || TranslationProvider.query('label-unknown-song'))} - ${(songInfo.artist || TranslationProvider.query('label-unknown-artist'))}` // eslint-disable-line
-  : windowTitle;
+
+const changeState = (stateVal) => {
+  const songInfo = PlaybackAPI.currentSong(true);
+  if (!songInfo) return;
+  let newString = `${(songInfo.title || TranslationProvider.query('label-unknown-song'))} - ${(songInfo.artist || TranslationProvider.query('label-unknown-artist'))}`; // eslint-disable-line
+  if (stateVal === 0) {
+    newString = windowTitle;
+  } else if (stateVal === 1) {
+    newString = `(Paused) ${newString}`;
+  }
   global.appIcon.setToolTip(newString);
   WindowManager.get(global.mainWindowID).setTitle(newString);
-});
+};
+
+Emitter.on('playback:isPlaying', changeState.bind(this, 2));
+Emitter.on('playback:isPaused', changeState.bind(this, 1));
+Emitter.on('playback:isStopped', changeState.bind(this, 0));
