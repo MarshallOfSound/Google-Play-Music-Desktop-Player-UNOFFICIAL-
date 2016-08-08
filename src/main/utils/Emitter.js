081c21b16eb3a523aa323e582a1bd76debcc1b21
Samuel Attard <samuel.r.attard@gmail.com>
2016-08-08 23:56:15 +1000

    Proxy all extra args in Emitters and spread in listeners to pass them through
    Fixes #1423

--- src/main/utils/Emitter.js
+++ src/main/utils/Emitter.js
@@ -4,58 +4,61 @@ import _ from 'lodash';
 class Emitter {
   constructor() {
     ipcMain.on('passback', (event, details) => {
-      this.sendToGooglePlayMusic(details.event, details.details);
+      this.sendToGooglePlayMusic(details.event, ...details.details);
     });
 
     ipcMain.on('passback:main', (event, details) => {
-      this.sendToWindowsOfName('main', details.event, details.details);
+      this.sendToWindowsOfName('main', details.event, ...details.details);
     });
 
     ipcMain.on('passback:all', (event, details) => {
-      this.sendToAll(details.event, details.details);
+      this.sendToAll(details.event, ...details.details);
     });
   }
 
-  _send(window, event, details = {}) {
+  _send(window, event, ...details) {
+    if (!details || details.length === 0) {
+      details = [{}];
+    }
     if (window.webContents.isLoading()) {
       let once = false;
       window.webContents.on('did-stop-loading', () => {
         if (!once) {
           once = true;
-          this._send(window, event, details);
+          this._send(window, event, ...details);
         }
       });
     } else {
-      window.webContents.send(event, details);
+      window.webContents.send(event, ...details);
     }
   }
 
-  sendToWindow(windowID, event, details) {
+  sendToWindow(windowID, event, ...details) {
     const window = WindowManager.get(windowID);
     if (window) {
-      this._send(window, event, details);
+      this._send(window, event, ...details);
     }
   }
 
-  sendToWindowsOfName(name, event, details) {
+  sendToWindowsOfName(name, event, ...details) {
     const windows = WindowManager.getAll(name);
     _.forEach(windows, (window) => {
       if (window) {
-        this._send(window, event, details);
+        this._send(window, event, ...details);
       }
     });
   }
 
-  sendToAll(event, details) {
+  sendToAll(event, ...details) {
     _.forIn(WindowManager.IDMap, (index, ID) => {
       const window = WindowManager.getByInternalID(ID);
       if (window) {
-        this._send(window, event, details);
+        this._send(window, event, ...details);
       }
     });
   }
 
-  sendToGooglePlayMusic(event, details) {
+  sendToGooglePlayMusic(event, ...details) {
     this.sendToWindowsOfName('main', 'passthrough', {
       event,
       details,
