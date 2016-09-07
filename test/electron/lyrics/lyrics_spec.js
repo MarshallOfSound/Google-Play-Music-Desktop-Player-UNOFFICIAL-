e25205bd337bad130cf474ef6cd0602dafd32d33
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-07 17:58:21 +1000

    Improve lyrics spec coverage

--- test/electron/lyrics/lyrics_spec.js
+++ test/electron/lyrics/lyrics_spec.js
@@ -1,5 +1,6 @@
 // Pre-run
 import chai from 'chai';
+import EventEmitter from 'events';
 import { givenAsync } from 'mocha-testdata';
 
 import attemptLyricsWikia from '../../../build/main/features/core/lyrics/source_lyricsWikia';
@@ -12,14 +13,29 @@ import { validSongs, invalidSongs } from '../testdata/lyrics';
 chai.should();
 
 // Mock expected globals
-global.PlaybackAPI = {
-  on: () => {},
+global.PlaybackAPI = new EventEmitter();
+let testSong = {
+  title: '(Just One Bracketted) One Call Away',
+  artist: 'Charlie Puth',
+  album: 'Nine Track Mind',
 };
 
 // Actual Test Imports
 const resolveLyrics = require('../../../build/main/features/core/lyrics').resolveLyrics; // eslint-disable-line
+const _playbackAPI = global.PlaybackAPI;
+delete global.PlaybackAPI;
 
 describe('Lyrics Resolver', () => {
+  before(() => {
+    global.PlaybackAPI = _playbackAPI;
+    PlaybackAPI.currentSong = () => testSong;
+    PlaybackAPI._setPlaybackSongLyrics = (lyrics) => PlaybackAPI.emit('change:lyrics', lyrics);
+  });
+
+  beforeEach(() => {
+    PlaybackAPI.removeAllListeners('change:lyrics');
+  });
+
   givenAsync(...validSongs).it('should resolve when given a valid song object', (done, song) => {
     resolveLyrics(song)
       .then((lyrics) => {
@@ -30,17 +46,34 @@ describe('Lyrics Resolver', () => {
   });
 
   it('should resolve when given a valid song object with brackets', (done) => {
-    const song = {
-      title: '(Just One Bracketted) One Call Away',
-      artist: 'Charlie Puth',
-      album: 'Nine Track Mind',
-    };
-    resolveLyrics(song)
+    resolveLyrics(testSong)
       .then((lyrics) => {
         lyrics.should.be.a('string');
         done();
       })
-      .catch(() => done(new Error(`Failed to fetch lyrics for song: ${song.title}`)));
+      .catch(() => done(new Error(`Failed to fetch lyrics for song: ${testSong.title}`)));
+  });
+
+  it('should change lyrics when recieving a PlaybackAPI event', (done) => {
+    PlaybackAPI.on('change:lyrics', (lyrics) => {
+      lyrics.should.be.ok; // eslint-disable-line
+      lyrics.should.be.a('string');
+      done();
+    });
+    PlaybackAPI.emit('change:track', testSong);
+  });
+
+  it('should not change lyrics when recieving a PlaybackAPI event if the current song has changed', (done) => {
+    PlaybackAPI.on('change:lyrics', (lyrics) => {
+      lyrics.should.be.a('string');
+      lyrics.split('I\'m only one call away').length.should.be.gt(2);
+      done();
+    });
+    const originalSong = Object.assign({}, testSong);
+    testSong = validSongs[1];
+    PlaybackAPI.emit('change:track', testSong);
+    testSong = originalSong;
+    PlaybackAPI.emit('change:track', testSong);
   });
 
   givenAsync(...invalidSongs).it('should fail when given an invalid song object', (done, song) => {
