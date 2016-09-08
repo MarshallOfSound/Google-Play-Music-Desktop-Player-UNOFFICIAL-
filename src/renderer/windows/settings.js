500bfcb965a9cfe26fe57f0cde1ddb63cb666316
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-08 23:42:19 +1000

    Fix the stupid thingy bug of bugginess

--- src/renderer/windows/settings.js
+++ src/renderer/windows/settings.js
@@ -1,10 +1,11 @@
+import { remote } from 'electron';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import SettingsPage from '../../ui/pages/SettingsPage';
+import SettingsPage from '../ui/pages/SettingsPage';
 
 injectTapEventPlugin();
 
 ReactDOM.render(<SettingsPage />, document.querySelector('#settings-window'));
-require('electron').remote.getCurrentWindow().show();
+remote.getCurrentWindow().show();
