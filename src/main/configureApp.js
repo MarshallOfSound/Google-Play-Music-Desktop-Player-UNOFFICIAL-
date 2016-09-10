ae85bdab7d70e4c6d57f24555605986e0dc75397
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-10 15:02:02 +1000

    Always disable GPU acceleration

--- src/main/configureApp.js
+++ src/main/configureApp.js
@@ -3,13 +3,12 @@ import path from 'path';
 export default (app) => {
   app.commandLine.appendSwitch('enable-smooth-scrolling', '1');
   app.commandLine.appendSwitch('enable-overlay-scrollbar', '1');
+  app.commandLine.appendSwitch('disable-gpu', '1');
   // DEV: Enables the HTML5 WebAudio API extensions to allow selection of sinkId
   //      --> Choosing audio output device
   app.commandLine.appendSwitch('enable-experimental-web-platform-features', '1');
 
-  if (process.platform === 'linux') {
-    app.disableHardwareAcceleration();
-  }
+  app.disableHardwareAcceleration();
 
   if (process.platform === 'darwin' && global.DEV_MODE) {
     app.dock.setIcon(path.resolve(__dirname, '..', 'assets/img/main.png'));
