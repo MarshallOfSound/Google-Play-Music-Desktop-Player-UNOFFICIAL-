9045595cd082178c068f6e80efd34e160419ad3a
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-13 02:51:47 +1000

    Use sentry to monitor in-app errors

--- src/main/features/core/errorHandler.js
+++ src/main/features/core/errorHandler.js
@@ -1,15 +1,35 @@
-process.on('uncaughtException', (error) => {
-  if (Logger) {
-    Logger.error('Uncaught Exception.', error);
-  } else {
-    // In case an exception was thrown before the logger was initialized.
-    console.log('Uncaught Exception: %j', error); // eslint-disable-line
-  }
-  if (global.DEV_MODE) return;
-  Emitter.sendToGooglePlayMusic('error', {
-    error: {
-      message: error.message,
-      stack: error.stack,
+import { app } from 'electron';
+import os from 'os';
+import raven from 'raven';
+
+if (process.env.GPMDP_SENTRY_DSN) {
+  const client = new raven.Client(process.env.GPMDP_SENTRY_DSN, {
+    release: app.getVersion(),
+    environment: global.DEV_MODE ? 'development' : 'production',
+    platform: os.platform(),
+    platform_version: os.release(),
+    arch: os.arch(),
+    system_memoty: os.totalmem(),
+    extra: {
+      settings: Settings.data,
     },
   });
-});
+  if (global.Logger) Logger.info('Starting Sentry');
+  client.patchGlobal();
+} else {
+  process.on('uncaughtException', (error) => {
+    if (global.Logger) {
+      Logger.error('Uncaught Exception.', error);
+    } else {
+      // In case an exception was thrown before the logger was initialized.
+      console.log('Uncaught Exception: %j', error); // eslint-disable-line
+    }
+    if (global.DEV_MODE) return;
+    Emitter.sendToGooglePlayMusic('error', {
+      error: {
+        message: error.message,
+        stack: error.stack,
+      },
+    });
+  });
+}
