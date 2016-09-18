31e335e07e5bb309f7095ade79edda0abd3307f0
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-18 23:09:44 +1000

    Fix lyricsWikia source

--- src/main/features/core/lyrics/source_lyricsWikia.js
+++ src/main/features/core/lyrics/source_lyricsWikia.js
@@ -9,15 +9,19 @@ const attemptLyricsWikia = (path) =>
     fetch(`http://lyrics.wikia.com/wiki/${path}`)
       .then((data) => data.text())
       .then((html) => {
-        let lyrics = (/('|")lyricbox('|")>(.+<\/script>)?(.+)<!--/g.exec(html)[4]);
-        lyrics = lyrics.replace(/<br \/>/gi, '\n');
-        lyrics = decoder.decode(lyrics);
-        lyrics = xss(lyrics, {
-          whiteList: { br: [], i: [], b: [], strong: [], em: [] },
-          stripIgnoreTag: true,
-          stripIgnoreTagBody: ['script'],
-        });
-        resolve(lyrics);
+        try {
+          let lyrics = (/('|")lyricbox('|")>(.+<\/script>)?(.+)(<!--)?/g.exec(html)[4]);
+          lyrics = lyrics.replace(/<br \/>/gi, '\n');
+          lyrics = decoder.decode(lyrics);
+          lyrics = xss(lyrics, {
+            whiteList: { br: [], i: [], b: [], strong: [], em: [] },
+            stripIgnoreTag: true,
+            stripIgnoreTagBody: ['script'],
+          });
+          resolve(lyrics);
+        } catch (err) {
+          reject(err);
+        }
       })
       .catch(reject);
   });
