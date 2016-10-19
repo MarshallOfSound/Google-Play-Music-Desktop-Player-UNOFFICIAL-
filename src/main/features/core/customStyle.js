f28561f4dcc6280c6f1b3bc3bbee3beb18209433
Samuel Attard <samuel.r.attard@gmail.com>
2016-10-20 00:11:16 +1100

    Handle dumb paths being given to the custom CSS tooling

--- src/main/features/core/customStyle.js
+++ src/main/features/core/customStyle.js
@@ -2,12 +2,18 @@ import fs from 'fs';
 
 Emitter.on('FetchMainAppCustomStyles', () => {
   const mainAppPath = Settings.get('mainAppStyleFile');
-  const css = mainAppPath && fs.existsSync(mainAppPath) ? fs.readFileSync(mainAppPath, 'utf8') : '';
-  Emitter.sendToAll('LoadMainAppCustomStyles', css);
+  if (mainAppPath && fs.existsSync(mainAppPath)) {
+    fs.readFile(mainAppPath, 'utf8', (err, css) => {
+      Emitter.sendToAll('LoadMainAppCustomStyles', err ? '' : css);
+    });
+  }
 });
 
 Emitter.on('FetchGPMCustomStyles', () => {
   const gpmPath = Settings.get('gpmStyleFile');
-  const css = gpmPath && fs.existsSync(gpmPath) ? fs.readFileSync(gpmPath, 'utf8') : '';
-  Emitter.sendToGooglePlayMusic('LoadGPMCustomStyles', css);
+  if (gpmPath && fs.existsSync(gpmPath)) {
+    fs.readFile(gpmPath, 'utf8', (err, css) => {
+      Emitter.sendToGooglePlayMusic('LoadGPMCustomStyles', err ? '' : css);
+    });
+  }
 });
