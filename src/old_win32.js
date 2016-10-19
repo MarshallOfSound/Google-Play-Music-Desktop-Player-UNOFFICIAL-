54a784b3ea3df03e2f63817ebdf4cef874b4b614
Samuel Attard <samuel.r.attard@gmail.com>
2016-10-20 00:06:16 +1100

    try {} catch {} the old_win32 check to ensure that syncronous errors are caught

--- src/old_win32.js
+++ src/old_win32.js
@@ -3,14 +3,19 @@
 import { exec } from 'child_process';
 
 if (process.platform === 'win32' && !Settings.get('checkedForOldVersion', false)) {
-  exec('wmic product where Vendor="MarshallOfSound" get PackageCache /format:value', (err, out) => {
-    Settings.set('checkedForOldVersion', true);
-    const MSIPath = out.replace(/(?:\r\n|\r|\n)/g, '').replace('PackageCache=', '');
-    if (MSIPath.split('.msi').length > 1) {
-      Emitter.once('uninstall:confirm', () => {
-        exec(`msiexec /uninstall ${MSIPath}`);
-      });
-      Emitter.sendToWindowsOfName('main', 'uninstall:request');
-    }
-  });
+  try {
+    exec('wmic product where Vendor="MarshallOfSound" get PackageCache /format:value', (err, out) => {
+      if (err) return;
+      Settings.set('checkedForOldVersion', true);
+      const MSIPath = out.replace(/(?:\r\n|\r|\n)/g, '').replace('PackageCache=', '');
+      if (MSIPath.split('.msi').length > 1) {
+        Emitter.once('uninstall:confirm', () => {
+          exec(`msiexec /uninstall ${MSIPath}`);
+        });
+        Emitter.sendToWindowsOfName('main', 'uninstall:request');
+      }
+    });
+  } catch (err) {
+    Logger.error('Failed to check for old version', err);
+  }
 }
