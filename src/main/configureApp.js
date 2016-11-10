d40d80c4617324a7a78fc41c9b111df5767d764b
Samuel Attard <samuel.r.attard@gmail.com>
2016-11-10 23:38:47 +1100

    Disable zoom-for-dfs.  Fixes #1777

--- src/main/configureApp.js
+++ src/main/configureApp.js
@@ -3,6 +3,7 @@ import path from 'path';
 export default (app) => {
   app.commandLine.appendSwitch('enable-smooth-scrolling', '1');
   app.commandLine.appendSwitch('enable-overlay-scrollbar', '1');
+  app.commandLine.appendSwitch('enable-use-zoom-for-dsf', 'false');
   app.commandLine.appendSwitch('disable-gpu', '1');
   // DEV: Enables the HTML5 WebAudio API extensions to allow selection of sinkId
   //      --> Choosing audio output device
