7802472fdb455aadc2648f70ca934bf5fa00fa41
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-29 01:40:07 +1100

    Add easy file to update translations

---
+++ vendor/update-translations.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+const locales = path.resolve(__dirname, '../src/_locales');
+
+const targetLines = fs.readFileSync(path.resolve(locales, 'en-US.json'), 'utf8').split('\n');
+
+const keyTest = () => /"(.+)":/gi;
+
+const targetKeys = [];
+for (let i = 0; i < targetLines.length; i++) {
+  if (targetLines[i].trim() === '{' || targetLines[i].trim() === '}' || !targetLines[i].trim()) continue;
+  targetKeys.push(keyTest().exec(targetLines[i])[1]);
+}
+
+
+fs.readdirSync(locales).forEach((fileName) => {
+  const needToAdd = [];
+  const filePath = path.resolve(locales, fileName);
+
+  if (fileName.endsWith('.json') && fileName !== 'en-US.json') {
+    const content = fs.readFileSync(filePath, 'utf8');
+
+    let lines = content.split('\n');
+    let keyI = 0;
+    let offset = 0;
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].trim() === '{' || lines[i].trim() === '}' || !lines[i].trim()) continue;
+      console.log(lines[i]);
+      const key = keyTest().exec(lines[i])[1];
+      if (key !== targetKeys[keyI]) {
+        needToAdd.push([i + offset, targetKeys[keyI]]);
+        keyI++;
+        offset++;
+      }
+      keyI++;
+    }
+
+    needToAdd.forEach((item) => {
+      lines = lines.slice(0, item[0] - 1).concat([`  "${item[1]}": "",`]).concat(lines.slice(item[0] - 1, lines.length));
+    });
+    fs.writeFileSync(filePath, lines.join('\n'));
+  }
+});
