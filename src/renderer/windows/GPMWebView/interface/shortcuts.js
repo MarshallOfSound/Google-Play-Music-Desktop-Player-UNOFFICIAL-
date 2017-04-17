122e0062e60f4351dd689a8515628f15443501bb
Samuel Attard <samuel.r.attard@gmail.com>
2017-04-17 16:20:29 +1200

    Add command+x shortcuts for the first 6 nav items.  Fixes #2376

--- src/renderer/windows/GPMWebView/interface/shortcuts.js
+++ src/renderer/windows/GPMWebView/interface/shortcuts.js
@@ -12,3 +12,15 @@ if (process.platform === 'win32') {
     }
   }, true);
 }
+
+if (process.platform === 'darwin') {
+  window.addEventListener('keydown', (e) => {
+    if (document.activeElement.value === undefined && e.metaKey && e.which >= 49 && e.which <= 54) {
+      const index = e.which - 49;
+      const navLinks = document.querySelectorAll('#nav a');
+      if (index < navLinks.length) {
+        navLinks[index].click();
+      }
+    }
+  }, true);
+}
