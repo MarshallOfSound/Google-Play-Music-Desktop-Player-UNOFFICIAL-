283fc83dc6b15fc57de5bc0973a948a15285d1b7
Scott Hardin <snhardin@users.noreply.github.com>
2017-06-01 15:28:22 -0700

    Only show window when not minimizing on startup.

--- src/index.js
+++ src/index.js
@@ -128,12 +128,11 @@ app.setAppUserModelId('com.marshallofsound.gpmdp.core');
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/public_html/index.html`);
 
-    // show the main window.
-    mainWindow.show();
-
     // minimize if 'start minimized' is on.
     if (Settings.get('startMinimized', false)) {
       mainWindow.minimize();
+    } else {
+      mainWindow.show();
     }
 
     require('./renderer/generic/translations');
