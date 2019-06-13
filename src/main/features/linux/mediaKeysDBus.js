9b48137303719c83f5ac98374a284c314b808e7c
Jesse Ostrander <jesse.ostrander@gmail.com>
2019-06-12 21:00:34 -0400

    Fix dbus media implementation. Now grabs media keys on window focus

--- src/main/features/linux/mediaKeysDBus.js
+++ src/main/features/linux/mediaKeysDBus.js
@@ -1,23 +1,30 @@
 import dbus from 'dbus-next';
+import { app } from 'electron';
 
-function registerBindings(desktopEnv, session) {
-  const listener = (err, iface) => {
-    if (!err) {
-      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
-        switch (keyName) {
-          case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
-          case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
-          case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
-          case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
-          default: return;
-        }
-      });
-      iface.GrabMediaPlayerKeys('GPMDP', 0); // eslint-disable-line
+async function registerBindings(desktopEnv, session) {
+  const listener = (n, keyName) => {
+    switch (keyName) {
+      case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
+      case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
+      case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
+      case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
+      default: return;
     }
   };
 
-  session.getInterface(`org.${desktopEnv}.SettingsDaemon`, `/org/${desktopEnv}/SettingsDaemon/MediaKeys`, `org.${desktopEnv}.SettingsDaemon.MediaKeys`, listener);
-  session.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`, `/org/${desktopEnv}/SettingsDaemon/MediaKeys`, `org.${desktopEnv}.SettingsDaemon.MediaKeys`, listener);
+  const legacy = await session.getProxyObject(`org.${desktopEnv}.SettingsDaemon`, `/org/${desktopEnv}/SettingsDaemon/MediaKeys`);
+  legacy.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`);
+  legacy.on('MediaPlayerKeyPressed', listener);
+  app.on('browser-window-focus', () => {
+    legacy.GrabMediaPlayerKeys('GPMDP', 0); // eslint-disable-line
+  });
+
+  const future = await session.getProxyObject(`org.${desktopEnv}.SettingsDaemon.MediaKeys`, `/org/${desktopEnv}/SettingsDaemon/MediaKeys`);
+  future.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`);
+  future.on('MediaPlayerKeyPressed', listener);
+  app.on('browser-window-focus', () => {
+    future.GrabMediaPlayerKeys('GPMDP', 0); // eslint-disable-line
+  });
 }
 
 try {
@@ -25,5 +32,5 @@ try {
   registerBindings('gnome', session);
   registerBindings('mate', session);
 } catch (e) {
-  // do nothing.
+  // Do nothing
 }
