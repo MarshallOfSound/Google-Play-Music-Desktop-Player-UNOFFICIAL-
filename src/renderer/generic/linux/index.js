c56fbf9bb7947bcf4f62df481dbf23b298a478bc
Jesse Ostrander <jesse.ostrander@gmail.com>
2019-06-13 10:35:16 -0400

    Upgrade to electron 4

--- src/renderer/generic/linux/index.js
+++ src/renderer/generic/linux/index.js
@@ -1,1 +1,9 @@
 import '../silent_Notification';
+import { remote } from 'electron';
+
+// remote.getCurrentWindow() polyfill?
+if (process.platform === 'linux') {
+  if (remote.getCurrentWindow() == null) {
+    remote.getCurrentWindow = () => remote.BrowserWindow.fromId(1);
+  }
+}
